c883b2904cd234ddce8e0a754f63a635dfdc70b1
erichiroshi <erichiroshi@gmail.com>
2023-07-18 10:46:05 -0400

    03.13 - Custom ConstraintValidator - email único - userinsertdto

--- backend/src/main/java/com/erichiroshi/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/erichiroshi/dscatalog/dto/UserInsertDTO.java
@@ -1,10 +1,13 @@
 package com.erichiroshi.dscatalog.dto;
 
+import com.erichiroshi.dscatalog.services.validation.UserInsertValid;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private String password;
