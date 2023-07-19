553e0c10b1cd28d5e7ee8f4272094905646611e1
erichiroshi <erichiroshi@gmail.com>
2023-07-19 13:05:21 -0400

    Variáveis de ambiente, Externalizando configuração

--- backend/src/main/java/com/erichiroshi/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/erichiroshi/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.erichiroshi.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -19,7 +23,7 @@ public class AppConfig {
 	@Bean
 	JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
